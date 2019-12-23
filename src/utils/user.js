4daeeeadc532db77df8ac7f82589ea4cddfebad0
berni <bernardo.llamas@sngular.com>
2019-12-23 11:08:06 +0100

    storing users 2

--- src/utils/user.js
+++ src/utils/user.js
@@ -1,7 +1,5 @@
 const users = [];
 
-// addUser, removeUser, getUser, getUsersInRoom
-
 const addUser = ({ id, username, room }) => {
     // Clean the data
     username = username.trim().toLowerCase();
@@ -40,16 +38,49 @@ const removeUser = (id) => {
     }
 }
 
-addUser({
+const getUser = (id) => {
+    return users.find((user) => user.id === id);
+}
+
+const getUsersInRoom = (room) => {
+    room = room.trim().toLowerCase();
+    return users.filter((user) => user.room === room);
+}
+
+module.exports = {
+    addUser,
+    removeUser,
+    getUser,
+    getUsersInRoom
+}
+
+/*addUser({
     id: 22,
     username: 'Bernie',
     room: 'Bornuard room'
 });
 
+addUser({
+    id: 44,
+    username: 'Bernie2',
+    room: 'Bornuard room '
+});
+
+addUser({
+    id: 66,
+    username: 'Bernie3',
+    room: 'Bornuard room 2'
+});
+
+const user = getUser(44);
+console.log(user);
+
+const userList = getUsersInRoom('Baxter room 2');
+console.log(userList);*/
+/*
 console.log(users);
-
 const removedUser = removeUser(22);
-
 console.log(removedUser);
 console.log(users);
+*/
 
